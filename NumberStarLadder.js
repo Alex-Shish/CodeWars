fe218a8078cc97671eca54b8fca0b00f378fc351
alexs <alexshishg@gmail.com>
2022-11-17 20:19:14 +0300

    add solution "Number-Star ladder"

---
+++ NumberStarLadder.js
@@ -0,0 +1,26 @@
+// Number-Star ladder
+// Task
+// Using n as a parameter in the function pattern, where n>0, complete the codes to get the pattern
+// (take the help of examples):
+// Note: There is no newline in the end (after the pattern ends)
+// Examples
+// pattern(3) should return "1\n1*2\n1**3", e.g. the following:
+// 1
+// 1*2
+// 1**3
+
+function pattern(n){
+    let output = '1';
+    for (let i = 2; i <= n; i++) {
+        output += '\n' + 1 + Array(i).join('*') + i;
+    }
+    return output;
+}
+console.log(pattern(7));
+// 1
+// 1*2
+// 1**3
+// 1***4
+// 1****5
+// 1*****6
+// 1******7
