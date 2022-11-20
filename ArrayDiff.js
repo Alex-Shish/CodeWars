e91ad80779bfb962ad46d6dcfd0b2f4db752a29e
alexs <alexshishg@gmail.com>
2022-11-20 18:18:01 +0300

    add solution "Array.diff"

--- ArrayDiff.js
+++ ArrayDiff.js
@@ -7,7 +7,7 @@
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 function arrayDiff(a, b) {
-    return a.filter(el => b.indexOf(el) === -1);
+return a.filter(el => b.indexOf(el) === -1);
 }
 
 console.log(arrayDiff([1, 2, 2, 2, 3], [2])); // [1, 3]
