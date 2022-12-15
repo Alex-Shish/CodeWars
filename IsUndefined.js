b0f79fba0896808507618433601df00878d877d5
alexs <alexshishg@gmail.com>
2022-12-15 08:47:52 +0300

    add solution "Is Undefined?"

---
+++ IsUndefined.js
@@ -0,0 +1,15 @@
+/* Is Undefined?
+Instructions:
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element
+on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+ */
+
+const isUndefined = value => value === undefined
+
+console.log(isUndefined(1)) // false
+console.log(isUndefined(2)) // false
+console.log(isUndefined(undefined)) //true
+console.log(isUndefined(3)) // false
+console.log(isUndefined(4)) // false
