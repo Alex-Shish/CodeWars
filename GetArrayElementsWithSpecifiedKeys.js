ca377b1926b5404058a87c946b4cadabed0e8bd1
alexs <alexshishg@gmail.com>
2022-12-18 14:53:09 +0300

    add solution "Get array elements with specified keys"

---
+++ GetArrayElementsWithSpecifiedKeys.js
@@ -0,0 +1,29 @@
+/* Get array elements with specified keys
+DESCRIPTION:
+Create a function that lets people easily grab only the array elements with given keys.
+For example:
+var array = ['a', 'b', 'c', 'd', 'e'];
+array2 = array.only([0, 3]);
+// at this point array2 should be ['a', 'd']
+Note: The order of the elements should be preserved in the resulting array.
+ */
+
+Array.prototype.only = function(keys)
+{
+    keys=keys.sort((a,b)=>a-b)
+    let arr= [];
+    for (let i=0;i<keys.length;i++)
+    {
+        arr.push(this[keys[i]])
+    }
+    return arr
+}
+console.log(['a', 'b', 'c', 'd', 'e'].only([0, 3])) // ['a', 'd']
+
+//OR
+
+Array.prototype.only = function(k) {
+    return this.filter((e, i) => k.includes(i));
+}
+
+console.log(['a', 'b', 'c', 'd', 'e'].only([0, 3])) // ['a', 'd']
