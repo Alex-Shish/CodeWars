cc40394f26d722c0371ddcd00b2a898276af3bb6
alexs <alexshishg@gmail.com>
2022-12-22 19:15:50 +0300

    add solution "How many arguments"

---
+++ HowManyArguments.js
@@ -0,0 +1,12 @@
+/* How many arguments
+DESCRIPTION:
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+ */
+
+function args_count() {
+    return arguments.length;
+}
+
+console.log(args_count(1, 2, 3)) //3
+console.log(args_count(1, 2, 3, 10)) // 4
