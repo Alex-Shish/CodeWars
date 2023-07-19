c0fd7e7d32df5892288d3346f02affa6625c0fcc
Alex-Shish <alexshishg@gmail.com>
2023-07-19 19:50:09 +0300

    add solution

--- PasswordValidator.js
+++ PasswordValidator.js
@@ -55,4 +55,8 @@ function password(str) {
       return count === 4;
     }
 
-    console.log(password("Abcd1234"));
+    console.log(password("Abcd1234"));                               // true
+    console.log(password("Abcd123"));                                // false
+    console.log(password("abcd1234"));                               // false
+    console.log(password("AbcdefGhijKlmnopQRsTuvwxyZ1234567890"));   // true
+    console.log(password("Ab1!@#$%^&*()-_+={}[]|\:;?/>.<,"));        // true
