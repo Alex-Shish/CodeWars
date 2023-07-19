c97ff58a12bea2d5746aec559358cc7f2b6bb836
Alex-Shish <alexshishg@gmail.com>
2023-07-19 19:41:28 +0300

    change 07-19-2023

--- PasswordValidator.js
+++ PasswordValidator.js
@@ -24,5 +24,35 @@ Extra info
 You will only be passed strings.
 The string can contain any standard keyboard character.
 Accepted strings can be any length, as long as they are 8 characters or more.
-
 */
+
+function password(str) {
+    let arr = str.split('');
+    console.log(arr)
+    let alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let alPh = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let count = 0;
+    if(arr.length >= 8) count++;
+    for(let i = 0; i < arr.length; i++){
+      if(alph.includes(arr[i])) {
+         count++;
+         break;
+         }
+      }
+    for(let i = 0; i < arr.length; i++){
+      if(alPh.includes(arr[i])){
+         count++;
+         break;
+         }
+    }
+    for(let i = 0; i < arr.length; i++){
+      if(nums.includes(arr[i])){
+         count++;
+         break;
+      }
+    }
+      return count === 4;
+    }
+
+    console.log(password("Abcd1234"));
