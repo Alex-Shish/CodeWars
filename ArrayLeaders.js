5875d91834ab94e7d4bdfaa66cdc0912c823809f
Alex-Shish <alexshishg@gmail.com>
2023-07-22 15:41:28 +0300

    change 07-22-2023

--- ArrayLeaders.js
+++ ArrayLeaders.js
@@ -20,10 +20,10 @@ function arrayLeaders(numbers){
 }
 
 
-console.log(arrayLeaders([1,2,3,4,0])); // [4]
-console.log(arrayLeaders([16,17,4,3,5,2])); // [17,5,2]
-console.log(arrayLeaders([-1,-29,-26,-2])); // [-1]
-console.log(arrayLeaders([-36,-12,-27])); // [-36,-12]
-console.log(arrayLeaders([5,-2,2])); // [5,2]
-console.log(arrayLeaders([0,-1,-29,3,2])); // [0,-1,3,2]
+console.log(arrayLeaders([1,2,3,4,0]));       //   [4]
+console.log(arrayLeaders([16,17,4,3,5,2]));   //   [17,5,2]
+console.log(arrayLeaders([-1,-29,-26,-2]));   //   [-1]
+console.log(arrayLeaders([-36,-12,-27]));     //   [-36,-12]
+console.log(arrayLeaders([5,-2,2]));          //   [5,2]
+console.log(arrayLeaders([0,-1,-29,3,2]));    //   [0,-1,3,2]
 
